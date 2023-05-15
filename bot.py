b17194f05b461996b39c7d3c93eaf8bb4a22dd2e
Johnny <qinjohnny1@gmail.com>
2023-05-16 00:47:14 +1000

    added more arguments

--- bot.py
+++ bot.py
@@ -130,6 +130,13 @@ async def safebooru_search(ctx, *, tags=""):
     # )
 
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
     options.add_argument("--disable-extensions")
     # options.binary_location = "./BraveSoftware/Brave-Browser/Application/brave.exe"
     options.add_argument("--headless")
@@ -200,6 +207,13 @@ async def danbooru_search(ctx, *, tags=""):
     # )
 
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
     options.add_argument("--disable-extensions")
     # options.binary_location = "./BraveSoftware/Brave-Browser/Application/brave.exe"
     options.add_argument("--headless")
